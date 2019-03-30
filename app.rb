b393021f278ca5f6d0e458800b38b1e553cd1bc2
oli-gra <oliver.grant@gmail.com>
2019-03-30 22:49:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is oli-gra"
+  end
+
+  get '/hometown' do
+    "My hometown is London"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bohemian Rhapsody"
+  end
 end
